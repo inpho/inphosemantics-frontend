e35a9f8878e74e4f94d249d5c472ff8bc248e8aa
Robert Rose <rrose1@indiana.edu>
2012-10-10 09:42:26 -0400

    Refining offline mode.

--- tinytornado.py
+++ tinytornado.py
@@ -11,62 +11,64 @@ stored_results = dict()
 exportQueryResults = dict()
 model_instance = dict()
 
-model_instances = {
-    ('sep', 'complete', 'beagle', 'environment'):
-        inpho.InphoViewer('sep', 'complete', 'beagleenvironment'),
-    ('sep', 'complete', 'beagle', 'context'):\
-        inpho.InphoViewer('sep', 'complete', 'beaglecontext'),
-    ('sep', 'complete', 'beagle', 'order'):\
-        inpho.InphoViewer('sep', 'complete', 'beagleorder'),
-    ('sep', 'complete', 'beagle', 'composite'):\
-        inpho.InphoViewer('sep', 'complete', 'beaglecomposite'),
-    ('sep', 'complete', 'tf', ''):
-        inpho.InphoViewer('sep', 'complete', 'tf'),
-    ('sep', 'complete', 'tfidf', ''):
-        inpho.InphoViewer('sep', 'complete', 'tf'),
+model_instances = {}
 
-    ('iep', 'complete', 'beagle', 'environment'):
-        inpho.InphoViewer('iep', 'complete', 'beagleenvironment'),
-    ('iep', 'complete', 'beagle', 'context'):
-        inpho.InphoViewer('iep', 'complete', 'beaglecontext'),
-    ('iep', 'complete', 'beagle', 'order'):
-        inpho.InphoViewer('iep', 'complete', 'beagleorder'),
-    ('iep', 'complete', 'beagle', 'composite'):
-        inpho.InphoViewer('iep', 'complete', 'beaglecomposite'),
-    ('iep', 'complete', 'tf', ''):
-        inpho.InphoViewer('iep', 'complete', 'tf'),
-    ('iep', 'complete', 'tfidf', ''):
-        inpho.InphoViewer('iep', 'complete', 'tfidf'),
-    
-    ('malaria', 'complete', 'beagle', 'environment'):
-        inpho.InphoViewer('malaria', 'complete', 'beagleenvironment'),
-    ('malaria', 'complete', 'beagle', 'context'):
-        inpho.InphoViewer('malaria', 'complete', 'beaglecontext'),
-    ('malaria', 'complete', 'beagle', 'order'):
-        inpho.InphoViewer('malaria', 'complete', 'beagleorder'),
-    ('malaria', 'complete', 'beagle', 'composite'):
-        inpho.InphoViewer('malaria', 'complete', 'beaglecomposite'),
-    ('malaria', 'complete', 'tf', ''):
-        inpho.InphoViewer('malaria', 'complete', 'tf'),
-    ('malaria', 'complete', 'tfidf', ''):
-        inpho.InphoViewer('malaria', 'complete', 'tfidf'),
-    
-    ('philpapers', 'complete', 'beagle', 'environment'):
-        inpho.InphoViewer('philpapers', 'complete', 'beagleenvironment'),
-    ('philpapers', 'complete', 'beagle', 'context'):
-        inpho.InphoViewer('philpapers', 'complete', 'beaglecontext'),
-    ('philpapers', 'complete', 'beagle', 'order'):
-        inpho.InphoViewer('philpapers', 'complete', 'beagleorder'),
-    ('philpapers', 'complete', 'beagle', 'composite'):
-        inpho.InphoViewer('philpapers', 'complete', 'beaglecomposite'),
+# model_instances = {
+#     ('sep', 'complete', 'beagle', 'environment'):
+#         inpho.InphoViewer('sep', 'complete', 'beagleenvironment'),
+#     ('sep', 'complete', 'beagle', 'context'):\
+#         inpho.InphoViewer('sep', 'complete', 'beaglecontext'),
+#     ('sep', 'complete', 'beagle', 'order'):\
+#         inpho.InphoViewer('sep', 'complete', 'beagleorder'),
+#     ('sep', 'complete', 'beagle', 'composite'):\
+#         inpho.InphoViewer('sep', 'complete', 'beaglecomposite'),
+#     ('sep', 'complete', 'tf', ''):
+#         inpho.InphoViewer('sep', 'complete', 'tf'),
+#     ('sep', 'complete', 'tfidf', ''):
+#         inpho.InphoViewer('sep', 'complete', 'tf'),
 
-    ## These don't exist, so leave them out until we've run these
-    ## and generated the data.
-#    ('philpapers', 'complete', 'tf', ''):
-#        inpho.InphoViewer('philpapers', 'complete', 'tf'),
-#    ('philpapers', 'complete', 'tfidf', ''):
-#        inpho.InphoViewer('philpapers', 'complete', 'tfidf')
-    }
+#     ('iep', 'complete', 'beagle', 'environment'):
+#         inpho.InphoViewer('iep', 'complete', 'beagleenvironment'),
+#     ('iep', 'complete', 'beagle', 'context'):
+#         inpho.InphoViewer('iep', 'complete', 'beaglecontext'),
+#     ('iep', 'complete', 'beagle', 'order'):
+#         inpho.InphoViewer('iep', 'complete', 'beagleorder'),
+#     ('iep', 'complete', 'beagle', 'composite'):
+#         inpho.InphoViewer('iep', 'complete', 'beaglecomposite'),
+#     ('iep', 'complete', 'tf', ''):
+#         inpho.InphoViewer('iep', 'complete', 'tf'),
+#     ('iep', 'complete', 'tfidf', ''):
+#         inpho.InphoViewer('iep', 'complete', 'tfidf'),
+    
+#     ('malaria', 'complete', 'beagle', 'environment'):
+#         inpho.InphoViewer('malaria', 'complete', 'beagleenvironment'),
+#     ('malaria', 'complete', 'beagle', 'context'):
+#         inpho.InphoViewer('malaria', 'complete', 'beaglecontext'),
+#     ('malaria', 'complete', 'beagle', 'order'):
+#         inpho.InphoViewer('malaria', 'complete', 'beagleorder'),
+#     ('malaria', 'complete', 'beagle', 'composite'):
+#         inpho.InphoViewer('malaria', 'complete', 'beaglecomposite'),
+#     ('malaria', 'complete', 'tf', ''):
+#         inpho.InphoViewer('malaria', 'complete', 'tf'),
+#     ('malaria', 'complete', 'tfidf', ''):
+#         inpho.InphoViewer('malaria', 'complete', 'tfidf'),
+    
+#     ('philpapers', 'complete', 'beagle', 'environment'):
+#         inpho.InphoViewer('philpapers', 'complete', 'beagleenvironment'),
+#     ('philpapers', 'complete', 'beagle', 'context'):
+#         inpho.InphoViewer('philpapers', 'complete', 'beaglecontext'),
+#     ('philpapers', 'complete', 'beagle', 'order'):
+#         inpho.InphoViewer('philpapers', 'complete', 'beagleorder'),
+#     ('philpapers', 'complete', 'beagle', 'composite'):
+#         inpho.InphoViewer('philpapers', 'complete', 'beaglecomposite'),
+
+#     ## These don't exist, so leave them out until we've run these
+#     ## and generated the data.
+# #    ('philpapers', 'complete', 'tf', ''):
+# #        inpho.InphoViewer('philpapers', 'complete', 'tf'),
+# #    ('philpapers', 'complete', 'tfidf', ''):
+# #        inpho.InphoViewer('philpapers', 'complete', 'tfidf')
+#     }
 
 
 
